3655dc540663e28a64f9befb0cd300dcbd30b434
mthtitumir <titumirgccc@gmail.com>
2024-01-08 22:14:35 +0600

    basic setup done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { StudentRoutes } from '../modules/student/student.route';
 import { UserRoutes } from '../modules/user/user.route';
 
 const router = Router();
@@ -8,10 +7,6 @@ const moduleRoutes = [
   {
     path: '/users',
     route: UserRoutes,
-  },
-  {
-    path: '/students',
-    route: StudentRoutes,
   },
 ];
 
