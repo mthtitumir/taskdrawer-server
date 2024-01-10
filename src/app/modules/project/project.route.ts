2fb74f3badf3365b5780ad3f36ea0b48308c4f41
mthtitumir <titumirgccc@gmail.com>
2024-01-10 23:09:28 +0600

    project create api polished

--- src/app/modules/project/project.route.ts
+++ src/app/modules/project/project.route.ts
@@ -3,7 +3,8 @@ import { ProjectControllers } from './project.controller';
 
 const router = express.Router();
 
-router.post('/', ProjectControllers.createProject);
-router.patch('/:projectId', ProjectControllers.updateProject);
+router.post('/', ProjectControllers.createProject); //authenticate user
+router.patch('/:projectId', ProjectControllers.updateProject); //authenticate user
+router.get('/', ProjectControllers.updateProject); //authenticate user(must)
 
 export const ProjectRoutes = router;
--- src/app/modules/project/project.service.ts
+++ src/app/modules/project/project.service.ts
@@ -1,16 +1,42 @@
+import mongoose from 'mongoose';
+import { User } from '../user/user.model';
 import { TProject } from './project.interface';
 import { Project } from './project.model';
 
 const createProjectIntoDB = async (payload: TProject) => {
-  const result = await Project.create(payload);
-  return result;
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+    const project = await Project.create([payload], { session });
+    // update user projects[],    
+    const updatedUser = await User.findByIdAndUpdate(
+      project[0].admin,
+      {
+        $push: { projects: project[0]?._id },
+      },
+      {
+        new: true,
+        runValidators: true,
+        session,
+      },
+    );
+    await session.commitTransaction();
+    await session.endSession();
+    return { project: project[0], updatedUser };
+  } catch (err:any) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error(err);
+  }
 };
 const updateProjectIntoDB = async (id: string, payload: Partial<TProject>) => {
   const result = await Project.findByIdAndUpdate(id, payload, { new: true });
   return result;
 };
+const getAllProjectFromDB = async () => {};
 
 export const ProjectServices = {
   createProjectIntoDB,
   updateProjectIntoDB,
+  getAllProjectFromDB,
 };
